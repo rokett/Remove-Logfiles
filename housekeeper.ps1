74f7d763ab82162f4e629cc9ab71d074e5171b81
Adam Nickells <adamnickells@gmail.com>
2014-07-16 23:35:58 +0100

    Logging completion of housekeeping task to Application log

--- housekeeper.ps1
+++ housekeeper.ps1
@@ -11,3 +11,12 @@ $date = (Get-Date).AddDays($days)
 
 # Delete files older than the $date.
 Get-ChildItem -Path $path -Include $extension | Where-Object {!$_.PSIsContainer -and $_.CreationTime -lt $date} | Remove-Item
+
+# Logging
+if (![System.Diagnostics.EventLog]::SourceExists('Housekeeper')) {
+    New-EventLog -LogName Application -Source Housekeeper
+}
+
+$message = 'Housekeeping complete for ' + $path
+
+Write-EventLog -LogName Application -Source Housekeeper -EntryType Information -EventId 1 -Message $message
